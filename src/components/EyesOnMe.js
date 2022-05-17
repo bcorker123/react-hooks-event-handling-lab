d9f9056e01446ac98e93d63f1e497b6d3ac3d22b
brian corker <bcorker123@gmail.com>
2022-05-17 17:26:08 -0400

    commit tree 3fec49389db8c803e9fce0d371f4781cdd8888f8

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,13 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+const handleFocus = ()=>console.log('Good!')
+const handleBlur = ()=>console.log('Hey! Eyes on me!')
+
+function EyesOnMe() {
+  return (
+    <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+  )
+}
+
+export default EyesOnMe
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,12 @@
 // Code Keypad Component Here
+import React from 'react'
+
+const handlePassword = ()=>console.log('Entering password...')
+
+function Keypad() {
+  return (
+    <input type='password' onChange={handlePassword}/>
+  )
+}
+
+export default Keypad
